61396fc21c148434d2f1d84726b5e7f48b902cb4
Daniel Dantas <xxxxxdanielzimmxxxx.danieldant@gmail.com>
2023-10-21 04:15:37 -0300

    novas funcionalidades

--- src/Components/Feed/Comments/Comments.jsx
+++ src/Components/Feed/Comments/Comments.jsx
@@ -1,12 +1,18 @@
 import React from 'react';
 import style from '../Comments/Comments.module.css';
 import H1 from '../../Form/H1';
+import {UserContext} from '../../../UserContext';
+import {ReactComponent as Dog} from '../../../../public/Assets/enviar.svg';
 
 const Comments = ({comentarios}) => {
 
-  React.useEffect(()=> {
-    console.log(comentarios)
-  }, [comentarios])
+  const {login , dataUser} = React.useContext(UserContext);
+
+  // React.useEffect(()=> {
+  //   console.log(comentarios)
+  //   console.log(dataUser)
+  //   console.log(login)
+  // }, [comentarios, dataUser, login])
 
   
   return (
@@ -14,13 +20,18 @@ const Comments = ({comentarios}) => {
       {comentarios && <>
         <div className={`${style.detalhes}  animationLeft`}>
         <div className={style.cabecalho}>
-          {comentarios && <>
-            <span className={style.autor}>@{comentarios.photo.author}</span>
+          {<>
+            {login ? 
+            (dataUser.username === comentarios.photo.author) ?      
+              <button className={style.btnDelete}>Deletar</button>: 
+              <span className={style.autor}>@{comentarios.photo.author}</span> 
+              : 
+              <span className={style.autor}>@{comentarios.photo.author}</span>}
             <span className={style.visualizacao}>{comentarios.photo.acessos}</span>
           </>}
         </div>
-        {comentarios && <H1 title={comentarios.photo.title}/>}
-        {comentarios && <div className={style.idadePeso}>
+        {<H1 title={comentarios.photo.title}/>}
+        {<div className={style.idadePeso}>
           <span>{comentarios.photo.peso} Kg</span>
           <span>{comentarios.photo.idade} {comentarios.photo.idade == 1 ? 'ano' : 'anos'}</span>
         </div>}
@@ -32,7 +43,13 @@ const Comments = ({comentarios}) => {
           </div>
         )}
       </div>
-      <div className={style.inputComment}></div>
+      {login && 
+        <div className={style.inputComment}>
+          <label htmlFor="comentario">
+            <textarea id='comentario' placeholder='Comente...'/>
+          </label>
+          <Dog/>
+        </div>}
       </>}
     </div>
   )
